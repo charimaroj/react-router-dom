dde14750135aa0859b680503db53ba7c2185e8c6
charimaroj <charimaroj@gmail.com>
2024-06-28 23:24:18 +0530

    vans, about pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Vans from "./pages/Vans";
+import "./index.css"
 
 function App() {
-
-
   return (
     <>
-  <h1>Welcome to React-Route-Dom</h1>
+      <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/vans" element={<Vans/>} />
+        </Routes>
+      </BrowserRouter>
+      <h1>Welcome to React-Route-Dom</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
